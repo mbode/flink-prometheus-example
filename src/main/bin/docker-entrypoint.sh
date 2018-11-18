d65e1be6e578753a96e5e956cfd83821b3dc25dd
Maximilian Bode <maximilian.bode@tngtech.com>
2018-11-18 21:00:23 +0100

    Use job-cluster mode for example job
    Remove second job

---
+++ src/main/bin/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+CMD="$1"
+shift;
+
+if [ "${CMD}" == "taskmanager" ]; then
+    exec $FLINK_HOME/bin/taskmanager.sh start-foreground "$@"
+elif [ "${CMD}" == "job-cluster" ]; then
+    exec $FLINK_HOME/bin/standalone-job.sh start-foreground "$@"
+fi
+
+exec "$@"
