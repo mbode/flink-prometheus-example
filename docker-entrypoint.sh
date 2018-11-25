c136d8059beef639553e3ce0acd4a47b787e700e
Maximilian Bode <maximilian.bode@tngtech.com>
2018-11-25 14:46:37 +0100

    Use shellcheck

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,10 @@
 CMD="$1"
 shift;
 
-if [ "${CMD}" == "taskmanager" ]; then
-    exec $FLINK_HOME/bin/taskmanager.sh start-foreground "$@"
-elif [ "${CMD}" == "job-cluster" ]; then
-    exec $FLINK_HOME/bin/standalone-job.sh start-foreground "$@"
+if [ "${CMD}" = "taskmanager" ]; then
+    exec "$FLINK_HOME/bin/taskmanager.sh" start-foreground "$@"
+elif [ "${CMD}" = "job-cluster" ]; then
+    exec "$FLINK_HOME/bin/standalone-job.sh" start-foreground "$@"
 fi
 
 exec "$@"
