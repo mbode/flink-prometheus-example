a989b5674956a9d67b7465eefae1adf6d57cfe89
Maximilian Bode <maximilian.bode@tngtech.com>
2019-02-03 14:01:53 +0100

    Upgrade various dependencies
    - test-sets 2.0.3 -> 2.1.1
    - shadow 4.0.3 -> 4.0.4
    - spotless 3.16.0 -> 3.17.0
    - docker-compose 0.8.12 -> 0.8.13
    - mockito-junit-jupiter 2.23.4 -> 2.24.0
    - awaitility 3.1.5 -> 3.1.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
     java
     jacoco
 
-    id("org.unbroken-dome.test-sets") version "2.0.3"
-    id("com.github.johnrengelman.shadow") version "4.0.3"
-    id("com.diffplug.gradle.spotless") version "3.16.0"
+    id("org.unbroken-dome.test-sets") version "2.1.1"
+    id("com.github.johnrengelman.shadow") version "4.0.4"
+    id("com.diffplug.gradle.spotless") version "3.17.0"
     id("com.github.spotbugs") version "1.6.9"
-    id("com.avast.gradle.docker-compose") version "0.8.12"
+    id("com.avast.gradle.docker-compose") version "0.8.13"
     id("com.github.ben-manes.versions") version "0.20.0"
 }
 
@@ -25,14 +25,14 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.apache.flink:flink-test-utils_2.11:$flinkVersion")
     testImplementation("org.assertj:assertj-core:3.11.1")
-    testImplementation("org.mockito:mockito-junit-jupiter:2.23.4")
+    testImplementation("org.mockito:mockito-junit-jupiter:2.24.0")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testRuntimeOnly("org.slf4j:slf4j-simple:1.7.25")
 
     val integrationTestImplementation by configurations
     integrationTestImplementation("com.mashape.unirest:unirest-java:1.4.9")
-    integrationTestImplementation("org.awaitility:awaitility:3.1.5")
+    integrationTestImplementation("org.awaitility:awaitility:3.1.6")
     integrationTestImplementation("com.github.docker-java:docker-java:3.0.14")
 }
 
