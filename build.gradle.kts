294865655e36ad4189203bea02ddb12b9e9262f8
Maximilian Bode <maximilian.bode@tngtech.com>
2019-10-05 21:31:18 +0200

    Upgrade various dependencies
    - test sets plugin 2.1.1 -> 2.2.0
    - spotless plugin 3.24.2 -> 3.24.3
    - docker-compose plugin 0.9.4 -> 0.9.5
    - mockito 3.0.0 -> 3.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
     java
     jacoco
 
-    id("org.unbroken-dome.test-sets") version "2.1.1"
+    id("org.unbroken-dome.test-sets") version "2.2.0"
     id("com.github.johnrengelman.shadow") version "5.1.0"
-    id("com.diffplug.gradle.spotless") version "3.24.2"
+    id("com.diffplug.gradle.spotless") version "3.24.3"
     id("com.github.spotbugs") version "2.0.0"
-    id("com.avast.gradle.docker-compose") version "0.9.4"
+    id("com.avast.gradle.docker-compose") version "0.9.5"
     id("com.github.ben-manes.versions") version "0.25.0"
 }
 
@@ -25,7 +25,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.apache.flink:flink-test-utils_2.11:$flinkVersion")
     testImplementation("org.assertj:assertj-core:3.13.2")
-    testImplementation("org.mockito:mockito-junit-jupiter:3.0.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:3.1.0")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testRuntimeOnly("org.slf4j:slf4j-simple:1.7.26")
