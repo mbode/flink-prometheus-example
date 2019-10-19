0ba3f2a4259f1ab577aed4ba5ff83887fd0e78ea
Maximilian Bode <maximilian.bode@tngtech.com>
2019-10-19 16:13:09 +0200

    Upgrade Flink 1.9.0 -> 1.9.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories { jcenter() }
 testSets { create("integrationTest") }
 
 dependencies {
-    val flinkVersion = "1.9.0"
+    val flinkVersion = "1.9.1"
     compileOnly("org.apache.flink:flink-java:$flinkVersion")
     compileOnly("org.apache.flink:flink-streaming-java_2.11:$flinkVersion")
 
