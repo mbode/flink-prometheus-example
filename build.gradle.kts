0df12b7070720bfa5a037187f5a4c8763519ad6b
Maximilian Bode <maxbode@gmail.com>
2020-04-13 18:39:08 +0200

    Upgrade various plugins and dependencies
    - test-sets plugin 2.2.1 -> 3.0.1
    - spotless plugin 3.26.1 -> 3.28.1
    - spotbugs plugin 3.0.0 -> 4.0.5
    - docker-compose plugin 0.10.7 -> 0.10.18
    - versions plugin 0.27.0 -> 0.28.0
    - junit 5.5.2 -> 5.6.1
    - assertj 3.14.0 -> 3.15.0
    - mockito 3.2.4 -> 3.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,13 @@
-import com.github.spotbugs.SpotBugsTask
-
 plugins {
     java
     jacoco
 
-    id("org.unbroken-dome.test-sets") version "2.2.1"
+    id("org.unbroken-dome.test-sets") version "3.0.1"
     id("com.github.johnrengelman.shadow") version "5.2.0"
-    id("com.diffplug.gradle.spotless") version "3.26.1"
-    id("com.github.spotbugs") version "3.0.0"
-    id("com.avast.gradle.docker-compose") version "0.10.7"
-    id("com.github.ben-manes.versions") version "0.27.0"
+    id("com.diffplug.gradle.spotless") version "3.28.1"
+    id("com.github.spotbugs") version "4.0.5"
+    id("com.avast.gradle.docker-compose") version "0.10.10"
+    id("com.github.ben-manes.versions") version "0.28.0"
 }
 
 repositories { jcenter() }
@@ -21,11 +19,11 @@ dependencies {
     compileOnly("org.apache.flink:flink-java:$flinkVersion")
     compileOnly("org.apache.flink:flink-streaming-java_2.11:$flinkVersion")
 
-    val junitVersion = "5.5.2"
+    val junitVersion = "5.6.1"
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.apache.flink:flink-test-utils_2.11:$flinkVersion")
-    testImplementation("org.assertj:assertj-core:3.14.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:3.2.4")
+    testImplementation("org.assertj:assertj-core:3.15.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:3.3.3")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testRuntimeOnly("org.slf4j:slf4j-simple:1.7.30")
@@ -33,17 +31,11 @@ dependencies {
     val integrationTestImplementation by configurations
     integrationTestImplementation("com.mashape.unirest:unirest-java:1.4.9")
     integrationTestImplementation("org.awaitility:awaitility:4.0.2")
-    integrationTestImplementation("com.github.docker-java:docker-java:3.1.2")
+    integrationTestImplementation("com.github.docker-java:docker-java:3.2.1")
 }
 
 tasks {
     withType(Test::class).configureEach { useJUnitPlatform() }
-    withType(SpotBugsTask::class).configureEach {
-        reports {
-            xml.isEnabled = false
-            html.isEnabled = true
-        }
-    }
     "jacocoTestReport"(JacocoReport::class) {
         reports {
             xml.isEnabled = true
