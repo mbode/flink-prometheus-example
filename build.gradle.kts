d402b489def97650e8fa4ee7104aba4a1dec0ed1
Maximilian Bode <maxbode@gmail.com>
2020-09-30 17:12:04 +0200

    Upgrade dependencies
    - Gradle 6.5.1 -> 6.6.1
    - Spotless plugin 5.1.0 -> 5.6.1
    - Spotbugs plugin 4.4.4 -> 4.5.1
    - docker-compose plugin 0.13.0 -> 0.13.3
    - versions plugin 0.29.0 -> 0.33.0
    - JUnit 5.6.1 -> 5.7.0
    - AssertJ 3.16.1 -> 3.17.2
    - Mockito 3.4.4 -> 3.5.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ plugins {
 
     id("org.unbroken-dome.test-sets") version "3.0.1"
     id("com.github.johnrengelman.shadow") version "6.0.0"
-    id("com.diffplug.spotless") version "5.1.0"
-    id("com.github.spotbugs") version "4.4.4"
-    id("com.avast.gradle.docker-compose") version "0.13.0"
-    id("com.github.ben-manes.versions") version "0.29.0"
+    id("com.diffplug.spotless") version "5.6.1"
+    id("com.github.spotbugs") version "4.5.1"
+    id("com.avast.gradle.docker-compose") version "0.13.3"
+    id("com.github.ben-manes.versions") version "0.33.0"
 }
 
 repositories { jcenter() }
@@ -19,11 +19,11 @@ dependencies {
     compileOnly("org.apache.flink:flink-java:$flinkVersion")
     compileOnly("org.apache.flink:flink-streaming-java_2.11:$flinkVersion")
 
-    val junitVersion = "5.6.1"
+    val junitVersion = "5.7.0"
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.apache.flink:flink-test-utils_2.11:$flinkVersion")
-    testImplementation("org.assertj:assertj-core:3.16.1")
-    testImplementation("org.mockito:mockito-junit-jupiter:3.4.4")
+    testImplementation("org.assertj:assertj-core:3.17.2")
+    testImplementation("org.mockito:mockito-junit-jupiter:3.5.13")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testRuntimeOnly("org.slf4j:slf4j-simple:1.7.30")
