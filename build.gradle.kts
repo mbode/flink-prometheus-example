e3db3613f7263e8a0228d9da9c14fd4a162f0631
pdys <przemek.dys@gmail.com>
2021-11-11 11:03:36 +0100

    Flink 1.13.3 and Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories { mavenCentral() }
 testSets { create("integrationTest") }
 
 dependencies {
-    val flinkVersion = "1.13.2"
+    val flinkVersion = "1.13.3"
     compileOnly("org.apache.flink:flink-java:$flinkVersion")
     compileOnly("org.apache.flink:flink-streaming-java_2.11:$flinkVersion")
 
